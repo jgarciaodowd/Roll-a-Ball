d6e4b3d9ed29e11237f97457f4a7bb4b01d4853f
Jaime <56158062+jgarciaodowd@users.noreply.github.com>
2021-01-23 11:14:50 +0100

    añadido el enemigo

---
+++ Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent pathfinder;
+    private Transform target;
+    
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        //goes after the game object Player
+        target = GameObject.Find("Player").transform;
+    }
+    void Update()
+    {
+        pathfinder.SetDestination(target.position);
+        //Debug.Log(target.position);
+    }
+}
