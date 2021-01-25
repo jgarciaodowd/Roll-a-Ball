c1ad1db9aeaee400b8725482a7d82d0709c0b54d
Jaime <56158062+jgarciaodowd@users.noreply.github.com>
2021-01-25 16:25:31 +0100

    enemigo añadido

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,71 +6,67 @@ using UnityEngine.InputSystem;
 using TMPro;
 
 public class PlayerController : MonoBehaviour
-{// Create public variables for player speed, and for the Text UI game objects
+{
+	// Create public variables for player speed, and for the Text UI game objects
 	public float speed;
-	public TextMeshProUGUI countText;
-	public GameObject winTextObject;
-
+    private Rigidbody rb;
+    private int count;
+    private int points;
     private float movementX;
     private float movementY;
+    private Vector3 OriginalPos;
+    public TextMeshProUGUI CountText;
+    public GameObject WinTextObject;
+    public GameObject LoseTextObject;
 
-	private Rigidbody rb;
-	private int count;
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>();
+        count = 0;
+        points = 0;
+        WinTextObject.SetActive(false);
+        LoseTextObject.SetActive(false);
+        setCountText();
+        OriginalPos = transform.position;
+    }
 
-	// At the start of the game..
-	void Start ()
-	{
-		// Assign the Rigidbody component to our private rb variable
-		rb = GetComponent<Rigidbody>();
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get < Vector2 >();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+        //movementX = Input.acceleration.x;
+        //movementY = -Input.acceleration.y;
+    }
 
-		// Set the count to zero 
-		count = 0;
-
-		SetCountText ();
-
-                // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
-                winTextObject.SetActive(false);
-	}
-
-	void FixedUpdate ()
-	{
-		// Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
-		Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-
-		rb.AddForce (movement * speed);
-	}
-
-	void OnTriggerEnter(Collider other) 
-	{
-		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
-		if (other.gameObject.CompareTag ("PickUp"))
-		{
-			other.gameObject.SetActive (false);
-
-			// Add one to the score variable 'count'
-			count = count + 1;
-
-			// Run the 'SetCountText()' function (see below)
-			SetCountText ();
-		}
-	}
-
-        void OnMove(InputValue value)
-        {
-        	Vector2 v = value.Get<Vector2>();
-
-        	movementX = v.x;
-        	movementY = v.y;
+    void setCountText(){
+        CountText.text = "Points: " + points.ToString();
+        if(count >= 8 && points == 8){
+            WinTextObject.SetActive(true);
+        }else{
+            if(count >= 8 && points < 8){
+                LoseTextObject.SetActive(true);
+            }
         }
+    }
 
-        void SetCountText()
-	{
-		countText.text = "Count: " + count.ToString();
+    void FixedUpdate(){
+        Vector3 movement = new Vector3(movementX,0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
 
-		if (count >= 12) 
-		{
-                    // Set the text value of your 'winText'
-                    winTextObject.SetActive(true);
-		}
-	}
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+            count++;
+            points++;
+            setCountText();
+        }
+        if(other.gameObject.CompareTag("Enemy")){
+            transform.position = OriginalPos;
+            if(points > 0){
+                points--;
+            }
+            setCountText();
+        }
+    }
 }
